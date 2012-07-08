e4ebd4857dcb763e1f4b42b1bb7bc3e6a450ab4e
Leonardo Cecchi <leonardoce@interfree.it>
2012-07-08 13:58:27 +0200

    access type

--- ada/fcgiapp_h.ads
+++ ada/fcgiapp_h.ads
@@ -3,7 +3,11 @@ with System;
 with Interfaces.C.Strings;
 
 package fcgiapp_h is
-
+   
+   
+   type FCGX_Stream;
+   type FCGX_Stream_Access is access FCGX_Stream;
+   
    --  unsupported macro: FCGX_UNSUPPORTED_VERSION -2
    --  unsupported macro: FCGX_PROTOCOL_ERROR -3
    --  unsupported macro: FCGX_PARAMS_ERROR -4
@@ -18,20 +22,20 @@ package fcgiapp_h is
       isClosed : aliased int;  -- c/fcgiapp.h:64
       wasFCloseCalled : aliased int;  -- c/fcgiapp.h:65
       FCGI_errno : aliased int;  -- c/fcgiapp.h:66
-      fillBuffProc : access procedure (arg1 : access FCGX_Stream);  -- c/fcgiapp.h:67
-      emptyBuffProc : access procedure (arg1 : access FCGX_Stream; arg2 : int);  -- c/fcgiapp.h:68
+      fillBuffProc : access procedure (arg1 : FCGX_Stream_Access);  -- c/fcgiapp.h:67
+      emptyBuffProc : access procedure (arg1 : FCGX_Stream_Access; arg2 : int);  -- c/fcgiapp.h:68
       data : System.Address;  -- c/fcgiapp.h:69
    end record;
    pragma Convention (C_Pass_By_Copy, FCGX_Stream);  -- c/fcgiapp.h:53
-
+   
    type FCGX_ParamArray is new System.Address;  -- c/fcgiapp.h:76
 
    type FCGX_Request is record
       requestId : aliased int;  -- c/fcgiapp.h:92
       role : aliased int;  -- c/fcgiapp.h:93
-      c_in : access FCGX_Stream;  -- c/fcgiapp.h:94
-      c_out : access FCGX_Stream;  -- c/fcgiapp.h:95
-      err : access FCGX_Stream;  -- c/fcgiapp.h:96
+      c_in : FCGX_Stream_Access;  -- c/fcgiapp.h:94
+      c_out : FCGX_Stream_Access;  -- c/fcgiapp.h:95
+      err : FCGX_Stream_Access;  -- c/fcgiapp.h:96
       envp : System.Address;  -- c/fcgiapp.h:97
       paramsPtr : System.Address;  -- c/fcgiapp.h:101
       ipcFd : aliased int;  -- c/fcgiapp.h:102
@@ -80,69 +84,69 @@ package fcgiapp_h is
    procedure FCGX_Finish;  -- c/fcgiapp.h:294
    pragma Import (C, FCGX_Finish, "FCGX_Finish");
 
-   function FCGX_StartFilterData (stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:313
+   function FCGX_StartFilterData (stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:313
    pragma Import (C, FCGX_StartFilterData, "FCGX_StartFilterData");
 
-   procedure FCGX_SetExitStatus (status : int; stream : access FCGX_Stream);  -- c/fcgiapp.h:328
+   procedure FCGX_SetExitStatus (status : int; stream : FCGX_Stream_Access);  -- c/fcgiapp.h:328
    pragma Import (C, FCGX_SetExitStatus, "FCGX_SetExitStatus");
 
    function FCGX_GetParam (name : Interfaces.C.Strings.chars_ptr; envp : FCGX_ParamArray) return Interfaces.C.Strings.chars_ptr;  -- c/fcgiapp.h:349
    pragma Import (C, FCGX_GetParam, "FCGX_GetParam");
 
-   function FCGX_GetChar (stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:369
+   function FCGX_GetChar (stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:369
    pragma Import (C, FCGX_GetChar, "FCGX_GetChar");
 
-   function FCGX_UnGetChar (c : int; stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:385
+   function FCGX_UnGetChar (c : int; stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:385
    pragma Import (C, FCGX_UnGetChar, "FCGX_UnGetChar");
 
    function FCGX_GetStr
      (str : Interfaces.C.Strings.chars_ptr;
       n : int;
-      stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:402
+      stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:402
    pragma Import (C, FCGX_GetStr, "FCGX_GetStr");
 
    function FCGX_GetLine
      (str : Interfaces.C.Strings.chars_ptr;
       n : int;
-      stream : access FCGX_Stream) return Interfaces.C.Strings.chars_ptr;  -- c/fcgiapp.h:421
+      stream : FCGX_Stream_Access) return Interfaces.C.Strings.chars_ptr;  -- c/fcgiapp.h:421
    pragma Import (C, FCGX_GetLine, "FCGX_GetLine");
 
-   function FCGX_HasSeenEOF (stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:442
+   function FCGX_HasSeenEOF (stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:442
    pragma Import (C, FCGX_HasSeenEOF, "FCGX_HasSeenEOF");
 
-   function FCGX_PutChar (c : int; stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:462
+   function FCGX_PutChar (c : int; stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:462
    pragma Import (C, FCGX_PutChar, "FCGX_PutChar");
 
    function FCGX_PutStr
      (str : Interfaces.C.Strings.chars_ptr;
       n : int;
-      stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:479
+      stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:479
    pragma Import (C, FCGX_PutStr, "FCGX_PutStr");
 
-   function FCGX_PutS (str : Interfaces.C.Strings.chars_ptr; stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:494
+   function FCGX_PutS (str : Interfaces.C.Strings.chars_ptr; stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:494
    pragma Import (C, FCGX_PutS, "FCGX_PutS");
 
-   function FCGX_FPrintF (stream : access FCGX_Stream; format : Interfaces.C.Strings.chars_ptr  -- , ...
+   function FCGX_FPrintF (stream : FCGX_Stream_Access; format : Interfaces.C.Strings.chars_ptr  -- , ...
       ) return int;  -- c/fcgiapp.h:510
    pragma Import (C, FCGX_FPrintF, "FCGX_FPrintF");
 
-   function FCGX_FFlush (stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:531
+   function FCGX_FFlush (stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:531
    pragma Import (C, FCGX_FFlush, "FCGX_FFlush");
 
-   function FCGX_FClose (stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:557
+   function FCGX_FClose (stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:557
    pragma Import (C, FCGX_FClose, "FCGX_FClose");
 
-   function FCGX_GetError (stream : access FCGX_Stream) return int;  -- c/fcgiapp.h:569
+   function FCGX_GetError (stream : FCGX_Stream_Access) return int;  -- c/fcgiapp.h:569
    pragma Import (C, FCGX_GetError, "FCGX_GetError");
 
-   procedure FCGX_ClearError (stream : access FCGX_Stream);  -- c/fcgiapp.h:580
+   procedure FCGX_ClearError (stream : FCGX_Stream_Access);  -- c/fcgiapp.h:580
    pragma Import (C, FCGX_ClearError, "FCGX_ClearError");
 
    function FCGX_CreateWriter
      (socket : int;
       requestId : int;
       bufflen : int;
-      streamType : int) return access FCGX_Stream;  -- c/fcgiapp.h:592
+      streamType : int) return FCGX_Stream_Access;  -- c/fcgiapp.h:592
    pragma Import (C, FCGX_CreateWriter, "FCGX_CreateWriter");
 
    procedure FCGX_FreeStream (stream : System.Address);  -- c/fcgiapp.h:608
